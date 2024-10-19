59fb95fb31f3d442d34bce0db88f296da54a40e2
Muhammadali00000 <muhammadalinarov@gmail.com>
2024-10-19 16:07:39 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='body'>
+      <div className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__link">
+              <a href="">Home</a>
+              <a href="">Location</a>
+              <a href="">Contact</a>
+            </div>
+            <img src="https://media.istockphoto.com/id/1337144146/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80-%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%B0-%D0%BF%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8F-%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%B0-%D0%BF%D0%BE-%D1%83%D0%BC%D0%BE%D0%BB%D1%87%D0%B0%D0%BD%D0%B8%D1%8E.jpg?s=612x612&w=0&k=20&c=fHyhvKma_mzzlFxVsuAoB7juqZOWt-ZUO56PRvkAO_c=" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <section className="bibendum">
+        <div className="container">
+          <div className="bibendum__wrapper">
+            <div className="bibendum__text">
+              <h1>
+              Bibendum et <br /> sit aliquam!
+              </h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sapien, est <br /> felis, sagittis viverra nulla mattis scelerisque. Eget cras integer.</p>
+            <button>
+              Discover Menu
+            </button>
+            </div>
+
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQAiSbUgqCbN_h3H7g5tjIZK4ljpN7cOAOFg&s" alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section className="our">
+        <div className="container">
+          <div className="our__wrapper">
+            <div className="card1">
+              <img src="https://img.freepik.com/premium-vector/sun-mountains-cartoon-mountains-with-sun-them-sunrise-mountains-vector-illustration_118339-6581.jpg" alt="" />
+              <h1>Neque volutpat morbi.</h1>
+              <p>Et blandit non sit ac egestas risus non.</p>
+              <button>Order Now</button>
+            </div>
+            <div className="card1">
+              <img src="https://img.freepik.com/premium-vector/sun-mountains-cartoon-mountains-with-sun-them-sunrise-mountains-vector-illustration_118339-6581.jpg" alt="" />
+              <h1>Neque volutpat morbi.</h1>
+              <p>Et blandit non sit ac egestas risus non.</p>
+              <button>Order Now</button>
+            </div>
+            <div className="card1">
+              <img src="https://img.freepik.com/premium-vector/sun-mountains-cartoon-mountains-with-sun-them-sunrise-mountains-vector-illustration_118339-6581.jpg" alt="" />
+              <h1>Neque volutpat morbi.</h1>
+              <p>Et blandit non sit ac egestas risus non.</p>
+              <button>Order Now</button>
+            </div>
+            <div className="card1">
+              <img src="https://img.freepik.com/premium-vector/sun-mountains-cartoon-mountains-with-sun-them-sunrise-mountains-vector-illustration_118339-6581.jpg" alt="" />
+              <h1>Neque volutpat morbi.</h1>
+              <p>Et blandit non sit ac egestas risus non.</p>
+              <button>Order Now</button>
+            </div>
+            <div className="card1">
+              <img src="https://img.freepik.com/premium-vector/sun-mountains-cartoon-mountains-with-sun-them-sunrise-mountains-vector-illustration_118339-6581.jpg" alt="" />
+              <h1>Neque volutpat morbi.</h1>
+              <p>Et blandit non sit ac egestas risus non.</p>
+              <button>Order Now</button>
+            </div>
+            <div className="card1">
+              <img src="https://img.freepik.com/premium-vector/sun-mountains-cartoon-mountains-with-sun-them-sunrise-mountains-vector-illustration_118339-6581.jpg" alt="" />
+              <h1>Neque volutpat morbi.</h1>
+              <p>Et blandit non sit ac egestas risus non.</p>
+              <button>Order Now</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App
